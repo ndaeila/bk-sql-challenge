8f147995bb5e6d9e1436774ea92ae96f7540609d
ndaeila <ndaeila@uw.edu>
2023-09-06 00:43:04 -0700

    Changes to table in challenge and gitignore

--- public/index.js
+++ public/index.js
@@ -19,7 +19,50 @@
       .then(resp => resp.text())
       .then(function(resp) {
         console.log(resp);
-        document.getElementById('output_field').textContent = resp;
+
+        let tableCreator = (function(resp) {
+          console.log(resp);
+          let table = document.createElement("table");
+          if (document.getElementById("output_field").innerHTML != null) {
+            document.getElementById("output_field").innerHTML = "";
+          }
+          table.setAttribute("id", "sql_table");
+          let header = document.createElement("tr");
+
+          let header0 = document.createElement("td");
+          header0.textContent = "Search";
+          let header1 = document.createElement("td");
+          header1.textContent = "Answer";
+
+          header.appendChild(header0);
+
+          header.appendChild(header1);
+
+          table.appendChild(header);
+
+          let searchList = JSON.parse(resp);
+
+          for (var i = 0; i < searchList.length; i++) {
+            let row = document.createElement("tr");
+
+            let search = document.createElement("td");
+            search.textContent = searchList[i]["search"];
+            let answer = document.createElement("td");
+            answer.textContent = searchList[i]["answer"];
+            row.appendChild(search);
+            row.appendChild(answer);
+
+            table.appendChild(row);
+          }
+          
+          if (searchList.length === 0) {
+            table.innerHTML = "";
+          }
+
+          return table;
+        });
+
+        document.getElementById('output_field').appendChild(tableCreator(resp));
       })
       .catch(resp => document.getElementById("output_field").textContent = resp);
   }
