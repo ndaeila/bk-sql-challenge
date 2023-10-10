c2108b87a9f80378f4b877ffda9efd38cb4e985d
ndaeila <ndaeila@uw.edu>
2023-10-10 13:45:04 -0700

    index.js changes

--- public/index.js
+++ public/index.js
@@ -4,75 +4,76 @@
   window.addEventListener('load', init);
 
   function init() {
-    document.getElementById("search_form").addEventListener("submit", async function(event) {
+    document.getElementById("search_form").addEventListener("submit", function(event) {
       event.preventDefault();
-      await getSearch(document.getElementById('search').value);
+
+      getSearch(document.getElementById('search').value);
     });
   }
 
-  async function getSearch(search) {
-    let params = new FormData;
+  function getSearch(search) {
+    let params = new FormData();
     params.append("search", search);
 
     fetch("/searchchallenge", {method: "POST", body: params})
       .then(statusCheck)
-      .then(resp => resp.text())
+      .then((resp) => resp.text())
       .then(function(resp) {
-        console.log(resp);
 
-        let tableCreator = (function(resp) {
-          console.log(resp);
-          let table = document.createElement("table");
-
-          table.setAttribute("id", "sql_table");
-          let header = document.createElement("tr");
-
-          let header0 = document.createElement("td");
-          header0.textContent = "Search";
-          let header1 = document.createElement("td");
-          header1.textContent = "Answer";
-
-          header.appendChild(header0);
-
-          header.appendChild(header1);
-
-          table.appendChild(header);
-
-          let searchList = JSON.parse(resp);
-
-          for (var i = 0; i < searchList.length; i++) {
-            let row = document.createElement("tr");
-
-            let search = document.createElement("td");
-            search.textContent = searchList[i]["search"];
-            let answer = document.createElement("td");
-            answer.textContent = searchList[i]["answer"];
-            row.appendChild(search);
-            row.appendChild(answer);
-
-            table.appendChild(row);
-          }
-
-          if (searchList.length === 0) {
-            table.innerHTML = "";
-          }
-
-          return table;
-        });
-
-        if (document.getElementById("output_field").innerHTML != null) {
+        if (document.getElementById("output_field").childNodes.length > 0) {
           document.getElementById("output_field").innerHTML = "";
         }
 
         document.getElementById('output_field').appendChild(tableCreator(resp));
       })
-      .catch(resp => document.getElementById("output_field").textContent = resp);
+      .catch((resp) => document.getElementById("output_field").textContent = resp);
+  }
+
+  function tableCreator(resp) {
+    let table = document.createElement("table");
+
+    table.setAttribute("id", "sql_table");
+    let header = document.createElement("tr");
+
+    let header0 = document.createElement("td");
+    header0.textContent = "Search";
+    let header1 = document.createElement("td");
+    header1.textContent = "Answer";
+
+    header.appendChild(header0);
+
+    header.appendChild(header1);
+
+    table.appendChild(header);
+
+    let searchList = JSON.parse(resp);
+
+    for (let i = 0; i < searchList.length; i++) {
+      let row = document.createElement("tr");
+
+      let search = document.createElement("td");
+      search.textContent = searchList[i]["search"];
+      let answer = document.createElement("td");
+      answer.textContent = searchList[i]["answer"];
+      row.appendChild(search);
+      row.appendChild(answer);
+
+      table.appendChild(row);
+    }
+
+    if (searchList.length === 0) {
+      table.innerHTML = "";
+    }
+
+    return table;
   }
 
   async function statusCheck(resp) {
     if (!resp.ok) {
       throw new Error(await resp.text());
     }
+
     return resp;
   }
+
 })();
